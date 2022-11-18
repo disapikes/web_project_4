1320ecd4e9c85862d0d16f38206a9fdf54e492d4
Pieter de Wet <disagecko@yahoo.co.uk>
2022-11-18 07:33:59 +0200

    transfer

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-function openForm() {
-  let popup = document.getElementById("popup");
+const cardTemplate = document.querySelector("#card").content;
+const cardContainer = document.querySelector(".cards");
+
+
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+/**
+ * 
+ * 
+ * function openForm() {
+  let popup = document.getElementById("editPopup");
   popup.classList.add("popup_opened");
 
   document.querySelector("#name").value = getDisplayName();
@@ -7,7 +42,7 @@ function openForm() {
 }
 
 function closeForm() {
-  let popup = document.getElementById("popup");
+  let popup = document.getElementById("editPopup");
   popup.classList.remove("popup_opened");
 }
 
@@ -19,9 +54,9 @@ function getDisplayRole() {
   let displayRole = document.querySelector(".profile__role").textContent;
   return displayRole;
 }
-let formElement = document.querySelector("#page");
-const closeBtn = document.querySelector("#closePopup");
-const openBtn = document.querySelector(".profile__edit-button");
+let editFormElement = document.querySelector("#editProfile");
+const closeEditBtn = document.querySelector("#closeEditPopup");
+const openEditBtn = document.querySelector(".profile__edit-button");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -35,7 +70,55 @@ function handleProfileFormSubmit(evt) {
   closeForm();
 }
 
-formElement.addEventListener("submit", handleProfileFormSubmit);
+editFormElement.addEventListener("submit", handleProfileFormSubmit);
 
-closeBtn.addEventListener("click", closeForm);
-openBtn.addEventListener("click", openForm);
+closeEditBtn.addEventListener("click", closeForm);
+openEditBtn.addEventListener("click", openForm);
+
+**/
+
+
+function openForm() {
+  let popup = document.getElementById("addPopup");
+  popup.classList.add("popup_opened");
+
+  document.querySelector("#title").value = "Title";
+  document.querySelector("#imglink").value = "Image link";
+}
+
+function closeForm() {
+  let popup = document.getElementById("addPopup");
+  popup.classList.remove("popup_opened");
+}
+
+
+let addFormElement = document.querySelector("#addPlace");
+const closeAddBtn = document.querySelector("#closeAddPopup");
+const openAddBtn = document.querySelector(".profile__add-button");
+
+function handlePlaceFormSubmit(evt) {
+  evt.preventDefault();
+
+  let titleInput = document.querySelector("#title").value;
+  let ImageInput = document.querySelector("#imglink").value;
+  createNewCard(titleInput, ImageInput) ;
+
+  closeForm();
+}
+
+addFormElement.addEventListener("submit", handlePlaceFormSubmit);
+
+closeAddBtn.addEventListener("click", closeForm);
+openAddBtn.addEventListener("click", openForm);
+
+function createNewCard(title, ima) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardContainer.append(cardElement);
+  const titleText = document.getElementById("label");
+  const  nameText= document.createTextNode(title);
+  titleText.append(nameText);
+  document.getElementById("image").src=ima;
+  
+  
+} 
+ 
